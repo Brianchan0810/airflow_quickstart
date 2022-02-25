841ce3a1145d8dc99beec475e003cf8b26ab7c48
brianchan <cxb2000abc@gmail.com>
2022-02-25 16:55:13 +0800

    commit

--- rdb_to_mongo2.py
+++ rdb_to_mongo2.py
@@ -8,30 +8,34 @@ from airflow.operators.python import PythonVirtualenvOperator
 
 def extract(**kwargs):
     print('currently inside extract task')
+
+    import json
+
     query = "Select * from MSP"
     mysql_hook = MySqlHook(schema='HTHK', mysql_conn_id='mysql_connection')
     connection = mysql_hook.get_conn()
     cursor = connection.cursor()
     cursor.execute(query)
 
-    data = cursor.fetchall()
-    columns_name = [item[0] for item in cursor.description]
+    raw_data = cursor.fetchall()
+    raw_columns_name = [item[0] for item in cursor.description]
+
+    data = json.dumps(raw_data)
+    columns_name = json.dumps(raw_columns_name)
 
     ti = kwargs['ti']
     ti.xcom_push('rdb_data', data)
     ti.xcom_push('columns_name', columns_name)
 
 
-def transform(xcom_data, xcom_columns_name):
+def transform(xcom_data, xcom_columns_name, **kwargs):
     print('currently inside etl task')
 
     import pandas as pd
+    import json
 
-    data = xcom_data
-    columns_name = xcom_columns_name
-
-    print(columns_name)
-    print(data)
+    data = json.loads(xcom_data)
+    columns_name = json.loads(xcom_columns_name)
 
     df = pd.DataFrame(data, columns=columns_name)
 
@@ -39,24 +43,26 @@ def transform(xcom_data, xcom_columns_name):
     basic_info['AGE_GRP'] = basic_info['AGE_BAND'].map({
         'band_01': '18-22', 'band_02': '22-30', 'band_03': '30-45', 'band_04': '45-60', 'band_05': '60 or above'})
     basic_info.drop(columns=['AGE_BAND'], inplace=True)
-    basic_info_dict = basic_info.to_dict('records')
-    print(basic_info_dict)
+    basic_info_json = json.dumps(basic_info.to_dict('records'))
 
-    #ti = kwargs['ti']
-    #ti.xcom_push('info_dict', basic_info_dict)
+    ti = kwargs['ti']
+    ti.xcom_push('basic_info', basic_info_json)
 
 def load(**kwargs):
     print('currently inside insert_data task')
 
+    import json
+
     ti = kwargs['ti']
-    info_dict = ti.xcom_pull(task_ids='transform', key='info_dict')
+    raw_basic_info = ti.xcom_pull(task_ids='transform', key='basic_info')
+    basic_info = json.loads(raw_basic_info)
 
     mongo_hook = MongoHook("mongo_connection")
     connection = mongo_hook.get_conn()
     db = connection['HTHK_test']
     collection = db['test']
 
-    collection.insert_many(info_dict)
+    collection.insert_many(basic_info)
 
     result = collection.find({}).limit(2)
     for item in result:
